e196a9427536da786a250429e68b61c929a71eb4
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-04-12 15:26:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
     document.getElementById("generateBtn").addEventListener("click", generatePassword);
     document.getElementById("copyIcon").addEventListener("click", copyPassword);
     document.getElementById("charLength").addEventListener("input", updateCharLengthValue);
+    document.getElementById("strength").addEventListener("input", updatePasswordStrength);
 }
 
 function generatePassword() {
@@ -32,11 +33,36 @@ function generatePassword() {
     }
 
     document.getElementById("pw").value = password;
+    updatePasswordStrength(); 
+}
+
+function updatePasswordStrength() {
+    var password = document.getElementById("pw").value;
+    var strengthBars = document.querySelectorAll(".strength-bar");
+
+    
+    for (var i = 0; i < strengthBars.length; i++) {
+        strengthBars[i].style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    }
+
+  
+    if (password.length >= 8) {
+        strengthBars[0].style.backgroundColor = "green";
+    }
+    if (password.length >= 12) {
+        strengthBars[1].style.backgroundColor = "green";
+    }
+    if (password.length >= 16) {
+        strengthBars[2].style.backgroundColor = "green";
+    }
+    if (password.length >= 20) {
+        strengthBars[3].style.backgroundColor = "green";
+    }
 }
 
 function updateCharLengthValue() {
     var sliderValue = document.getElementById("charLength").value;
-    document.getElementById("charLengthValue").textContent = sliderValue;
+    document.getElementById("charLengthValue").textContent = ` ${sliderValue} `;
 }
 
 function copyPassword() {
